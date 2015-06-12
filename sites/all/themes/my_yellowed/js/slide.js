1e143a7a7195e8ff7bfbf6f2d21dc0411550b682
Kelly de Vries <kdv@me.com>
2015-06-12 06:09:34 -0700

    menu update

---
+++ sites/all/themes/my_yellowed/js/slide.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * Yellowed Slider Javascript.
+ *
+ */
+(function ($) {
+Drupal.behaviors.slider = {
+attach: function (context, settings) {
+  $(window).load(function() {
+    $("#single-post-slider").flexslider({
+      animation: 'slide',
+      slideshow: true,
+      controlNav: true,
+      smoothHeight: true,
+      smoothWidth: true,
+      start: function(slider) {
+        slider.container.click(function(e) {
+          if(!slider.animating) {
+            slider.flexAnimate(slider.getTarget('next'));
+          }
+        });
+      }
+    });
+  });
+}
+};
+})(jQuery);
